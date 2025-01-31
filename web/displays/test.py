3d2bd370fa4d5500b74703b44db6fa3d1c181873
Ryan Smith <smithr3873@gmail.com>
2025-01-31 16:03:36 -0500

    move displays

--- web/displays/test.py
+++ web/displays/test.py
@@ -14,9 +14,14 @@ def signal_handler(sig, frame):
 signal.signal(signal.SIGINT, signal_handler)
 signal.signal(signal.SIGTERM, signal_handler)
 
-if __name__ == "__main__":
+
+def main():
     x = 0
     while running:
         print(f"Running for {x} second{'' if x == 1 else 's'}")
         x += 1
         time.sleep(1)
+
+
+if __name__ == "__main__":
+    main()
